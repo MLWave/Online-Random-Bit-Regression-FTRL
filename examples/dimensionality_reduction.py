1bd38162f03ba83675c4af6d9eea5a03b6d0170e
MLWave <info@mlwave.com>
2015-08-16 20:33:21 +0200

    prelim

---
+++ examples/dimensionality_reduction.py
@@ -0,0 +1,14 @@
+"""
+  Author: Triskelion, HJ van Veen, info@mlwave.com
+
+  Description:
+  
+  Dimensionality Reduction
+  
+  RBR can be used for dimensionality reduction.
+  
+  Either by trial or error, or saving by only the best most informative bits you can create short bitstrings that reduce dimensionality.
+  
+  Todo:
+    Clean code
+"""
