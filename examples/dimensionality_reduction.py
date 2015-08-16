eea188fc98fc2e1fb77c954268fdc9d0c6500130
MLWave <info@mlwave.com>
2015-08-16 20:38:06 +0200

    spelling

--- examples/dimensionality_reduction.py
+++ examples/dimensionality_reduction.py
@@ -7,7 +7,8 @@
   
   RBR can be used for dimensionality reduction.
   
-  Either by trial or error, or saving by only the best most informative bits you can create short bitstrings that reduce dimensionality.
+  Either by trial or error, or by saving only the best (most informative/highest weighted) bits, 
+  you can create short bitstrings that reduce dimensionality.
   
   Todo:
     Clean code
